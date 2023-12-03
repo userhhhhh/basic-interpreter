8c131f1da05aba2a1da57d7e6584bfd326e2272b
hqs123 <hqs123@sjtu.edu.cn>
2023-12-03 20:18:28 +0800

    commit 5

--- Basic/Basic.cpp
+++ Basic/Basic.cpp
@@ -100,7 +100,7 @@ void processLine(std::string line, Program &program, EvalState &state) {
                 stmt->execute(state,program);
                 delete stmt;
             }
-            catch(...){delete stmt;throw;}
+            catch(...){delete stmt;throw;}//错误
             return;
         }
     } else if (token == "PRINT") {
